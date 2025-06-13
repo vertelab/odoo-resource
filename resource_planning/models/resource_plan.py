98e4282aff9645d1ab5f95ccac70aba78dd4ef42
marcuszxc <marcus.karlsson@vertel.se>
2025-06-13 07:55:07 +0000

    resource_planning: Fixed a bug and added sepperators for filters in shift

--- resource_planning/models/resource_plan.py
+++ resource_planning/models/resource_plan.py
@@ -135,7 +135,7 @@ class ResourcePlan(models.Model):
             record.slot_count = self.env["resource.slot"].search_count([('plan_id', '=', record.id)])
 
     def action_get_shifts(self):
-        shifts = self.env["resource.shift"].search([("planning_id", "=", self.id)])
+        shifts = self.env["resource.shift"].search([("plan_id", "=", self.id)])
         shifts.recompute_stored_fields()
         action = {
             'type': 'ir.actions.act_window',
